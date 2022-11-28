c72c6bf0895576637cc9fc7f5414d63c83b93e6a
JJWER <119285215+metagorn@users.noreply.github.com>
2022-11-28 15:41:04 +0700

    add name id email

---
+++ lab OOP00A/lab OOP00A/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Metagorn Singkhan");
+Console.WriteLine("653450100-9");
+Console.WriteLine("metagorn.s@kkumail.com");
+
